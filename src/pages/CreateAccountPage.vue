24a4f27a34da093eaa4d1fcac078f69a3bc718b4
nikita.kukhar <nikita.kukhar@best2pay.net>
2022-11-19 23:28:01 +0300

    createAccount

--- src/pages/CreateAccountPage.vue
+++ src/pages/CreateAccountPage.vue
@@ -1,13 +1,78 @@
 <template>
-  <div>
-    CreateAccount
+  <div class="create-account">
+    <button
+      class="base-button base-button--accent"
+      style="margin-bottom: 24px"
+    >
+      назад
+    </button>
+    <h2>Создать счет</h2>
+    <p>У пользователя может быть только один счет в одной валюте</p>
+
+    <div class="create-account-group flex-col">
+      <div class="title">
+        Выберите валюту
+      </div>
+      <ul class="flex-col">
+        <li
+          class="accout-type"
+          :class="{'selected': selectedAccount === 840}"
+          @click="selectAccount(840)"
+        >
+          <img
+            src="~@/assets/img/dollar-icon.svg"
+            alt="currency-icon"
+          >
+          Доллар
+        </li>
+
+        <li
+          class="accout-type"
+          :class="{'selected': selectedAccount === 978}"
+          @click="selectAccount(978)"
+        >
+          <img
+            src="~@/assets/img/euro-icon.svg"
+            alt="currency-icon"
+          >
+          Евро
+        </li>
+      </ul>
+    </div>
+
+    <VButton @click="createAccount">создать счет</VButton>
+    <p class="condition">
+      Нажимая на кнопку “создать счет” вы соглашаетесь с 
+      <a
+        href="#"
+        target="_blank"
+      >условиями оферты</a> 
+    </p>
   </div>
 </template>
 <script>
+import VButton from '@/components/VButton/VButton.vue';
+import { ref } from '@vue/reactivity';
+
 export default {
-    name: 'CreateAccountPage'
+  name: "CreateAccountPage",
+  components: { VButton },
+  setup() {
+    const selectedAccount = ref(840)
+
+    function selectAccount(code) {
+      selectedAccount.value = code
+    }
+
+    function createAccount() {
+      alert(selectedAccount.value)
+    }
+    return {
+      selectedAccount,
+      selectAccount,
+      createAccount
+    }
+  }
 }
 </script>
-<style lang="">
-    
-</style>
+<style src="@/scss/createAccount.scss" lang="scss"></style>
